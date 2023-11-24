25beca719ae306cca23466c39c6f093458131f4a
Rohit Keshwani <rohit.keshwani@gmail.com>
2023-11-23 22:14:21 -0600

    Added Ability to pre-select recreational vehicle dimensions.

--- src/components/Roof.js
+++ src/components/Roof.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Unstable_Grid2'; // G
+import Grid from '@mui/material/Unstable_Grid2';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import rvData from '../data/rvData.json';
+import { InputLabel } from '@mui/material';
+
 const Roof = ({ dimensions, onDimensionsChange }) => {
+  const [selectedRV, setSelectedRV] = useState(null);
+
+  const handleRVChange = (event) => {
+    const rvId = event.target.value;
+    const selectedRV = rvData.find((rv) => rv.id === rvId);
+    setSelectedRV(selectedRV);
+    if (rvId !== '') {
+      onDimensionsChange(selectedRV.length, selectedRV.width);
+    }
+  };
+
   const handleLengthChange = (event) => {
     const length = parseInt(event.target.value);
     onDimensionsChange(length, dimensions.width);
@@ -17,6 +33,23 @@ const Roof = ({ dimensions, onDimensionsChange }) => {
     <>
       <Grid xs={12}>
         <Typography variant="h2">Roof</Typography>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <InputLabel id="rv-select-label">Select a Recreational Vehicle or enter Custom dimensions</InputLabel>
+        <Select
+          id="rv"
+          labelId='rv-select-label'
+          value={selectedRV ? selectedRV.id : ''}
+          onChange={handleRVChange}
+          
+        >
+          <MenuItem value="">Custom</MenuItem>
+          {rvData.map((rv) => (
+            <MenuItem key={rv.id} value={rv.id}>
+              {rv.name}
+            </MenuItem>
+          ))}
+        </Select>
       </Grid>
       <Grid item xs={12} md={6}>
         <TextField
