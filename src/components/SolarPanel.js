db16f45cb64610ef6c046f5501ade9cb2491daee
Rohit Keshwani <rohit.keshwani@gmail.com>
2023-12-08 21:53:41 -0600

    Added initial solar repository

--- src/components/SolarPanel.js
+++ src/components/SolarPanel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,6 +7,8 @@ import CardHeader from '@mui/material/CardHeader';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import { MenuItem, Select } from '@mui/material';
+import solarPanelList from '../data/solarPanels.json';
 
 const SolarPanel = ({ index, length, width, powerCapacity, onRemove, onUpdate, updateGrid }) => {
   const handleLengthChange = (event) => {
@@ -30,11 +32,39 @@ const SolarPanel = ({ index, length, width, powerCapacity, onRemove, onUpdate, u
     onRemove(index);
   };
 
+  // const filePath = './data/solarPanels.json';
+
+  // const getSolarPanels = async () => {
+  //   try {
+  //     console.log('Reading solarPanels.json...');
+  //     const response = await fetch(filePath);
+  //     const solarPanels = await response.json();
+  //     return solarPanels;
+  //   } catch (error) {
+  //     console.error('Error reading solarPanels.json:', error);
+  //     return [];
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getSolarPanels().then((solarPanels) => setSolarPanelList(solarPanels));
+  // }, []);
+  const handleOnSolarTemplateChange = (event) => {
+    //find solar panel in solarPanelList
+    const templateValue = solarPanelList.find((solarPanel) => solarPanel.name === event.target.value);
+    //update length, width, powerCapacity
+    onUpdate(index, templateValue.length, templateValue.width, templateValue.powerOutput);
+  };
   return (
     <Card>
       <CardHeader title={`Solar Panel ${index + 1}`} />
       <CardContent>
         <Stack spacing={2}>
+          <Select onChange={handleOnSolarTemplateChange}>
+            {solarPanelList.map((solarPanel, index) => (
+              <MenuItem key={index} value={solarPanel.name}>{solarPanel.name}</MenuItem>
+            ))}
+          </Select>
           <TextField
             type="number"
             id={`length-${index}`}
